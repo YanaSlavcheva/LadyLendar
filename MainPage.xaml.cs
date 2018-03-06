5cb52d81a0ed459a77174a046de5efa2f312b712
YanaSlavcheva <yana.slavcheva@gmail.com>
2018-03-06 21:52:25 +0200

    removed commented code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,14 +12,14 @@
 
     public sealed partial class MainPage : Page
     {
-        private DateTimeOffset newlyAddedPeriodStartDate = new DateTimeOffset(DateTime.MinValue, TimeSpan.Zero);
+        ////private DateTimeOffset newlyAddedPeriodStartDate = new DateTimeOffset(DateTime.MinValue, TimeSpan.Zero);
         private IList<PeriodInfoItem> periodData = new List<PeriodInfoItem>();
         //private int periodLength = Int32.MinValue;
 
         public MainPage()
         {
             this.InitializeComponent();
-            this.NewlyAddedPeriodStartDate = newlyAddedPeriodStartDate;
+            ////this.NewlyAddedPeriodStartDate = newlyAddedPeriodStartDate;
             lstMachineFunctions.ItemsSource = periodsData = new ObservableCollection<PeriodInfoItem>();
         }
 
@@ -27,32 +27,52 @@
 
         public ObservableCollection<PeriodInfoItem> periodsData { get; set; }
 
-        private async void periodStartsBtn_ClickAsync(object sender, RoutedEventArgs e)
+        ////private async void periodStartsBtn_ClickAsync(object sender, RoutedEventArgs e)
+        ////{
+        ////    var mediaElement = new MediaElement();
+        ////    var synth = new SpeechSynthesizer();
+        ////    var message = "Please, choose period starting date";
+
+        ////    if (this.newlyAddedPeriodStartDate != new DateTimeOffset(DateTime.MinValue, TimeSpan.Zero))
+        ////    {
+        ////        message = $"You set your period to start from { this.newlyAddedPeriodStartDate.ToString("d", CultureInfo.InvariantCulture) }";
+        ////        periodsData.Add(new PeriodInfoItem
+        ////        {
+        ////            periodStartDateValue = this.newlyAddedPeriodStartDate,
+        ////            periodStartDateValueToString = this.newlyAddedPeriodStartDate.ToString("d", CultureInfo.InvariantCulture),
+        ////            periodEndDateValue = this.newlyAddedPeriodStartDate.AddDays(5),
+        ////            periodEndDateValueToString = this.newlyAddedPeriodStartDate.AddDays(5).ToString("d", CultureInfo.InvariantCulture)
+        ////        });
+        ////    }
+
+        ////    var stream = await synth.SynthesizeTextToStreamAsync(message);
+        ////    mediaElement.SetSource(stream, stream.ContentType);
+        ////    mediaElement.Play();
+        ////}
+
+        private async void DatePicker_DateChanged(object sender, DatePickerValueChangedEventArgs e)
         {
+            var newlyAddedPeriodStartDate = e.NewDate;
+
             var mediaElement = new MediaElement();
             var synth = new SpeechSynthesizer();
             var message = "Please, choose period starting date";
 
-            if (this.newlyAddedPeriodStartDate != new DateTimeOffset(DateTime.MinValue, TimeSpan.Zero))
+            if (newlyAddedPeriodStartDate != new DateTimeOffset(DateTime.MinValue, TimeSpan.Zero))
             {
-                message = $"You set your period to start from { this.newlyAddedPeriodStartDate.ToString("d", CultureInfo.InvariantCulture) }";
+                message = $"You set your period to start from { newlyAddedPeriodStartDate.ToString("d", CultureInfo.InvariantCulture) }";
                 periodsData.Add(new PeriodInfoItem
                 {
-                    periodStartDateValue = this.newlyAddedPeriodStartDate,
-                    periodStartDateValueToString = this.newlyAddedPeriodStartDate.ToString("d", CultureInfo.InvariantCulture),
-                    periodEndDateValue = this.newlyAddedPeriodStartDate.AddDays(5),
-                    periodEndDateValueToString = this.newlyAddedPeriodStartDate.AddDays(5).ToString("d", CultureInfo.InvariantCulture)
+                    periodStartDateValue = newlyAddedPeriodStartDate,
+                    periodStartDateValueToString = newlyAddedPeriodStartDate.ToString("d", CultureInfo.InvariantCulture),
+                    periodEndDateValue = newlyAddedPeriodStartDate.AddDays(5),
+                    periodEndDateValueToString = newlyAddedPeriodStartDate.AddDays(5).ToString("d", CultureInfo.InvariantCulture)
                 });
             }
 
             var stream = await synth.SynthesizeTextToStreamAsync(message);
             mediaElement.SetSource(stream, stream.ContentType);
             mediaElement.Play();
-        }
-
-        private void DatePicker_DateChanged(object sender, DatePickerValueChangedEventArgs e)
-        {
-            this.newlyAddedPeriodStartDate = e.NewDate;
         }
     }
 
